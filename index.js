e49576125beebf0e2c6693ee51f6d475e5c4bac8
Christopher <c.barnhurst@live.com>
2021-09-02 11:43:33 -0500

    commit tree 523f68b41f399502dd1655c16dd9585bd5854ff6

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const num3 = 5
+const num4 = 24
+const set = (5, 10, 15, 20)
 
-// Write your code here
+let multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = (num4 % num3)
+
+let max = Math.max(set)
